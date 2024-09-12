374bd92465099a05ae3c943200f3ca4302d29579
Nathan Yu <nathanyu@Nathans-MacBook-Pro.local>
2024-09-12 16:15:47 -0400

    test: testing vscode, added .DS_Store in gitignore for mac

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,8 +7,10 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("hello world");
         
     }
+
 
     // Update is called once per frame
     void Update()
