6b90a4c2b708180ad052139b99d108fbe3c259e9
Oliver Long <longoliver2005@gmail.com>
2024-09-13 19:04:37 -0400

    Creating Scripts

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    //Placeholder
+    //[SerializeField] Enemy spawnableEnemies = {};
+    [SerializeField] int[] enemyWeights;
+
+
+    float timeTillNextWave;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnWave() {
+        //enemiesPerWave = {};
+    }
+}
