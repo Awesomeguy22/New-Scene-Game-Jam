1a60681bcd3320a5a90ef5ded0f62b10a42c0470
Nathan Yu <nathanyu@pal-10-184-43-191.itap.purdue.edu>
2024-09-14 14:14:21 -0400

    temp: tentacle

---
+++ Assets/Scripts/Tentacle.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Tentacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateTentacle();
+    }
+
+    private void RotateTentacle() {
+        Vector2 mouseCoordinates = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3 tentacleCoordinates = gameObject.transform.position;
+
+        // Vector3 pointingVector = new Vector3(mouseCoordinates.x, mouseCoordinates.y, 0) - new Vector3(tentacleCoordinates.x, tentacleCoordinates.y, 0);
+        Vector2 pointingVector = mouseCoordinates - new Vector2(tentacleCoordinates.x, tentacleCoordinates.y);
+        // Debug.Log(Mathf.Atan(pointingVector.y / pointingVector.x) * Mathf.Rad2Deg);
+
+        float angle = Vector2Deg(pointingVector) + 90;
+
+
+
+
+        Quaternion pointingAngle = Quaternion.Euler(0, 0, angle);
+
+        Quaternion currentAngle = Quaternion.Euler(0, 0, transform.rotation.z);
+        gameObject.transform.rotation = Quaternion.Lerp(currentAngle, pointingAngle, Time.deltaTime);
+        // transform.rotation = Quaternion.Euler(0, 0, angle);
+        Debug.Log("Pointing: " + pointingAngle.eulerAngles + " Current: " + currentAngle.eulerAngles + " lerp: " + transform.rotation.eulerAngles);
+    }
+
+    private float Vector2Deg(Vector2 vector) {
+        float angle = Mathf.Atan(vector.y / vector.x) * Mathf.Rad2Deg;
+
+        if (vector.x < 0 && vector.y > 0) {
+            angle += 180;
+        };
+        
+        if (vector.x < 0 && vector.y < 0) {
+            angle -= 180;
+        }
+
+        return angle;
+    }
+}
