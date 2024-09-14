e440150ae85af2154c4eaa9377613a693e74c551
Oliver Long <longoliver2005@gmail.com>
2024-09-13 22:21:45 -0400

    Test Scene created

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Spawns an enemy wave each time with a certain weight
     
+    [SerializeField] GameManager gameManager;
     //Placeholder
-    //[SerializeField] Enemy spawnableEnemies = {};
+    [SerializeField] GameObject[] spawnableEnemies;
     [SerializeField] int[] enemyWeights;
 
+    //for each difficulty level, a list of enemy waves to spawn
+    [SerializeField] int[][] possibleEnemyWaves;
 
+    float timeBetweenWaves = 10;
     float timeTillNextWave;
+
+    [SerializeField] int waveWeights;
     void Start()
     {
         
+    }
+
+    void Awake(){
+        if (!gameManager){
+            gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+
+        }
+        timeTillNextWave = timeBetweenWaves;
     }
 
     // Update is called once per frame
@@ -24,6 +39,18 @@ public class EnemyManager : MonoBehaviour
     }
 
     void SpawnWave() {
+        timeTillNextWave = timeBetweenWaves;
+        int randomStage = Random.Range(1,possibleEnemyWaves[gameManager.currentGameStage].Length);
+        int[] wave = possibleEnemyWaves[randomStage];
+        for (int i = 0; i < spawnableEnemies.Length; i++){
+            int spawnCount = wave[i];
+
+            //create a spawncount number of enemy i 
+            for (int j = 0; j < spawnCount; j++) {
+                //create enemy i 
+                //Instantiate()
+            }
+        }
         //enemiesPerWave = {};
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,14 @@ public class GameManager : MonoBehaviour
     public int playerHealth;
 
     private int playerXP = 0;
-    [SerializeField] int[] XPthresholds = {};
+    [SerializeField] int[] ExpThresholds = {};
     
     //int from 1-4 representing the phase of the game
     //Used by spawner and attack logic
     public int currentGameStage = 1;
 
+    [SerializeField] int winStage = 5;
+    [SerializeField] Scene winScreen;
 
     void Start()
     {
@@ -28,8 +30,12 @@ public class GameManager : MonoBehaviour
 
 
     void CheckGamePhase() {
-        if (playerXP > XPthresholds[currentGameStage - 1]) {
+        if (playerXP > ExpThresholds[currentGameStage - 1]) {
             currentGameStage += 1;
+        }
+
+        if (currentGameStage == winStage){
+            Win();
         }
     }
 
@@ -43,6 +49,10 @@ public class GameManager : MonoBehaviour
     void RestartGame() {
         Debug.Log("You Lost");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Win(){
+        SceneManager.LoadScene(winScreen.name);
     }
     void DamagePlayer(int damage){
         playerHealth -= damage;
