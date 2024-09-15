fcc9f62eb71c637ba706d2e68651d3c257013f6f
Nathan Yu <nathanyu@pal-10-184-29-137.itap.purdue.edu>
2024-09-15 13:42:21 -0400

    feat: added bgm/effect sound volumes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -8,9 +9,21 @@ public class AudioManager : MonoBehaviour
     private AudioClip[] audioFiles;
     [SerializeField]
     [Range(0, 1)]
-    private float volume = 1;
+    private float masterVolume = 1;
+    [SerializeField]
+    [Range(0, 1)]
+    private float bgmVolume = 1;
+    [SerializeField]
+    [Range(0, 1)]
+    private float effectsVolume = 1;
 
-    private AudioSource audioSource;
+    private AudioSource[] audioSources = new AudioSource[2];
+
+    public enum VolumeType {
+        master, 
+        bgm,
+        effect,
+    }
 
     // enter audio clip name to use when calling clip, order has to be the same as the order in audioFiles
     public enum ClipName {
@@ -23,8 +36,8 @@ public class AudioManager : MonoBehaviour
 
     private void Awake() {
         DontDestroyOnLoad(gameObject);
-        this.audioSource = FindObjectOfType<AudioSource>();
-        this.audioSource.volume = volume;
+        this.audioSources = FindObjectsByType<AudioSource>(FindObjectsSortMode.None);
+        this.audioSources[0].volume = masterVolume * bgmVolume;
     }
 
     private void Start() {
@@ -32,25 +45,46 @@ public class AudioManager : MonoBehaviour
     }
     
     public void PlayAudioClip(ClipName clip) {
-        this.audioSource.PlayOneShot(audioFiles[(int)clip]); 
+        this.audioSources[1].PlayOneShot(audioFiles[(int)clip]); 
     }
 
     public void PlayBGM(ClipName clip) {
-        this.audioSource.clip = audioFiles[(int)clip];
-        this.audioSource.loop = true;
-        this.audioSource.Play();
+        this.audioSources[0].clip = audioFiles[(int)clip];
+        this.audioSources[0].loop = true;
+        this.audioSources[0].Play();
         
     }
 
-    public void ChangeVolume(float volume) {
+    public void ChangeVolume(float volume, VolumeType volumeType) {
         if (volume > 1) {
             return;
         }
 
-        this.audioSource.volume = volume;
+
+        switch (volumeType) {
+            case VolumeType.master:
+                this.masterVolume = volume;
+                break;
+
+            case VolumeType.bgm:
+                this.bgmVolume = volume;
+                break;
+
+            case VolumeType.effect:
+                this.effectsVolume = volume;
+                break;
+            default: break;
+        }
+        this.audioSources[0].volume = masterVolume * bgmVolume;
+        this.audioSources[1].volume = masterVolume * effectsVolume;
     }
 
-    public float GetVolume() {
-        return this.audioSource.volume;
+    public float GetVolume(VolumeType volumeType) {
+        switch (volumeType) {
+            case VolumeType.master: return this.masterVolume; 
+            case VolumeType.bgm: return this.bgmVolume;
+            case VolumeType.effect: return this.effectsVolume;
+            default: return 0;
+        }
     }
 }
